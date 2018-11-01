179298c77b5c34f1450e953b230490e5824e9a19
System Administrator <root@flomacbook.local>
2018-11-01 10:55:47 +0100

    zeugs hochladen

---
+++ script.js
@@ -0,0 +1,12 @@
+function box1(){
+	document.body.style.backgroundImage = "url('s.jpg')";
+}
+function box2(){
+	document.body.style.backgroundImage = "url('mac.jpg')";
+}
+function box3(){
+	document.body.style.backgroundImage = "url('p.jpg')";
+}
+function reset() {
+	document.body.style.backgroundImage = "url('board.jpg')";
+}
